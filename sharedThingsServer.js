b4aaa7be6aa67deade858b6b81f86b21dd1c846e
Jiang Sheng <gisonrg@gmail.com>
2014-04-20 05:59:36 +0800

    Hahahahahahaahahh

--- sharedThingsServer.js
+++ sharedThingsServer.js
@@ -4,15 +4,15 @@ var config = require('./config_node.js');
 
 var WebSocketServer = require('ws').Server, wss = new WebSocketServer({port: config.port});
 function NewWord() {
-	this.world = {
-              "x1":{"x":70,"y":112.5}, "x2":{"x":70,"y":112.5}, "x3":{"x":70,"y":112.5},
-              "x4":{"x":70,"y":112.5}, "x5":{"x":70,"y":112.5}, "o1":{"x":70,"y":112.5},
-              "o2":{"x":70,"y":112.5}, "o3":{"x":70,"y":112.5}, "o4":{"x":70,"y":112.5},
-              "o5":{"x":70,"y":112.5}, "tttboard":{"x":70,"y":112.5}
-            };
+	this.world = {"x1":{"x":8,"y":8}, "x2":{"x":8,"y":51}, "x3":{"x":8,"y":94},
+             "x4":{"x":8,"y":137}, "x5":{"x":8,"y":180}, "o1":{"x":260,"y":8},
+             "o2":{"x":260,"y":51}, "o3":{"x":260,"y":94}, "o4":{"x":260,"y":137},
+             "o5":{"x":260,"y":180},"tttboard":{"x":70,"y":30}};
 }
 
 var server_world = {};
+
+var world_location = {};
 
 var serverNameList=[];
 
@@ -51,11 +51,12 @@ wss.on('connection', function(ws) {
 		// if get the reset order, reset and broadcast the initial world
 		if (changed.type == "reset") {
 			// create a new world
+			console.log(changed.world);
 			var newWorld = new NewWord();
 			server_world[changed.world] = newWorld.world;
 
 			var initial = JSON.stringify({type:"initial", world: changed.world, data:JSON.stringify(newWorld.world)});
-			wss.broadcast(reset);
+			wss.broadcast(initial);
 		}
 
 		if (changed.type == "create") {
@@ -63,6 +64,16 @@ wss.on('connection', function(ws) {
 				// prepare a new world data
 				var newWorld = new NewWord();
 				server_world[changed.world] = newWorld.world;
+
+				// get newly connected user's location information
+				var geoLocation = JSON.parse(changed.gps);
+				if (geoLocation.lat!=undefined && geoLocation.lng!=undefined) {
+					// store the location information
+					world_location[changed.world] =[];
+					world_location[changed.world].push(geoLocation);
+
+					
+				}
 				// update the world list data		
 				serverNameList.push(changed.world);
 				// sending the world name list
@@ -82,10 +93,28 @@ wss.on('connection', function(ws) {
 
 		if (changed.type == "join") {
 			// a user want to join a existing world
+			console.log(JSON.stringify(changed));
 			// first pull out the world data
 			var data = server_world[changed.world];
+			// get newly connected user's location information
+			var geoLocation = JSON.parse(changed.gps);
+			if (geoLocation.lat!=undefined && geoLocation.lng!=undefined) {
+				// keep other users update
+				wss.broadcast(JSON.stringify({type:"geo", world:changed.world, data:[changed.gps]}));
+			}
+			
 			// send a success message
 			ws.send(JSON.stringify({type:"success", world:changed.world, data:"Join successfully."}));
+			
+			// push the updated clients location to the users
+			ws.send(JSON.stringify({type:"geo", world:changed.world, data:JSON.stringify(world_location[changed.world])}));
+
+			if (geoLocation.lat!=undefined && geoLocation.lng!=undefined) {
+				// store the location information
+				world_location[changed.world].push(geoLocation);
+			}
+			
+
 			// once create the world, send the world to the user
 			var initial = JSON.stringify({type:"initial", world: changed.world, data:JSON.stringify(data)});
 			//Send the initial position to the newly connected client.
